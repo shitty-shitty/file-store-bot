f3325684345c5e6d3142f7a3b79a68c391b3efe0
Bookworm2024 <infohubnetworks@gmail.com>
2024-10-15 16:07:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -194,8 +194,8 @@ async def start_command(client: Client, message: Message):
                     link = await get_shortlink(SHORTLINK_API_URL, SHORTLINK_API_KEY,f'{newLink}')
                     if USE_PAYMENT:
                         btn = [
-                        [InlineKeyboardButton("ᴠᴇʀɪꜰʏ 🎀", url=link)
-                        InlineKeyboardButton('ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ 🥲', url=TUT_VID)],
+                        [InlineKeyboardButton("ᴠᴇʀɪꜰʏ 🎀", url=link)],
+                        [InlineKeyboardButton('ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ 🥲', url=TUT_VID)],
                         [InlineKeyboardButton("Buy Premium plan", callback_data="buy_prem")]
                         ]
                     else:
@@ -246,8 +246,8 @@ async def start_command(client: Client, message: Message):
             link = await get_shortlink(SHORTLINK_API_URL, SHORTLINK_API_KEY,f'https://telegram.dog/{client.username}?start=verify_{token}')
             if USE_PAYMENT:
                 btn = [
-                [InlineKeyboardButton("ᴠᴇʀɪꜰʏ 🎀", url=link),
-                InlineKeyboardButton('ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ 🥲', url=TUT_VID)],
+                [InlineKeyboardButton("ᴠᴇʀɪꜰʏ 🎀", url=link)],
+                [InlineKeyboardButton('ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ 🥲', url=TUT_VID)],
                 [InlineKeyboardButton("Buy Premium plan", callback_data="buy_prem")]
                 ]
             else:
